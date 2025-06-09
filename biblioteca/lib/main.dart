17e6028b52fe62c25d714169042d5732620104d0
Luis Mendoza <mendozalucho826@gmail.com>
2025-06-09 08:47:45 -0500

    modificacion del main

--- biblioteca/lib/main.dart
+++ biblioteca/lib/main.dart
@@ -1,24 +1,4 @@
 import 'package:flutter/material.dart';
-import 'features/home/presentation/views/success/principal_screen.dart'; // Asegúrate que esta ruta sea correcta.
+import 'features/home/presentation/views/success/principal_screen.dart'; 
 
-void main() {
-  runApp(const BookApp());
-}
-
-class BookApp extends StatelessWidget {
-  const BookApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Búsqueda de Libros',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: PrincipalScreen(),
-    );
-  }
-}
 
