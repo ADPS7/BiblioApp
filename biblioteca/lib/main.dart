412d6ab6e529bed04aa0d0af3addf439f1c6aa26
Luis Mendoza <mendozalucho826@gmail.com>
2025-06-09 08:54:48 -0500

    resolviendo problemas

--- biblioteca/lib/main.dart
+++ biblioteca/lib/main.dart
@@ -1,4 +1,20 @@
+import 'biblioteca/features/home/presentation/views/loadingview.dart';
 import 'package:flutter/material.dart';
 import 'features/home/presentation/views/success/principal_screen.dart'; 
+
+void main(){
+  runApp(Lib());
+}
+
+class Lib extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Biblioteca Portatil',
+      debugShowCheckedModeBanner: false,
+      home: Loadingini(),
+    );
+  }
+}
 
 
